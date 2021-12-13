5c3bc38bb6bd3837ede0235a9a4cac9e50afd337
Amod Mathur <amodmathur104@gmail.com>
2021-12-13 01:00:08 -0500

    fixed mongodb host

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,2 +1,2 @@
 SECRET_KEY = b'<\xef\x94\xa7\xcad%\x17\x19\x1a\x8f\xa5\xa5\xe6F\xf4'
-MONGODB_HOST = "mongodb://localhost:27017/final-proj"
+MONGODB_HOST = "mongodb+srv://JDqZGfywQwtyTIGP:JDqZGfywQwtyTIGP@cmsc388-cluster.5kn19.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
