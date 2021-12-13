95782413616b5e2589fd98abc24662ce3db67d59
Amod Mathur <amodmathur104@gmail.com>
2021-12-12 20:25:49 -0500

    againnnn

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -35,14 +35,14 @@ def page_not_found(e):
 
 
 
-def create_app():
+def create_app(test_config=None):
     app = Flask(__name__)
+    app.config["MONGODB_HOST"] = os.getenv("MONGODB_HOST")
 
     app.config.from_pyfile("config.py", silent=False)
-    # if test_config is not None:
-    #     app.config.update(test_config)
+    if test_config is not None: 
+        app.config.update(test_config)
 
-    app.config["MONGODB_HOST"] = os.getenv("MONGODB_HOST")
 
     db.init_app(app)
     login_manager.init_app(app)
